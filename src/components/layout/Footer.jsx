58e240bc93ef5877c86d501257b9ca84366d2262
ian <onlordev@gmail.com>
2023-01-25 20:21:20 +0900

    style: footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 const Foot = styled.footer`
-  margin-top: 100px;
-  text-align: end;
+  position: absolute;
+  bottom: 0;
   color: #d7d7d7;
   border-top: 1px solid #d7d7d7;
 `;
